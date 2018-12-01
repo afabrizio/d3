b220f8bd31dfb4396ed6a0a72cae6b690a8f083e
cytellix-dev <afabrizio@cytellix.com>
2018-11-30 18:01:28 -0800

    Initialized project with npm and webpack.

---
+++ hierarchy.js
@@ -0,0 +1,4 @@
+// http://bl.ocks.org/NPashaP/7683252
+const svg = document.createElement('svg');
+svg.setAttribute('height', 800);
+svg.setAttribute('width', 800);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const header = document.createElement('h1');
+header.textContent = 'Andrew\'s d3 Library';
+document.body.appendChild(header);_
