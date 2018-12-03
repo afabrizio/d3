1157fa3649af446a921af52ca56c386cb8641e53
andrew <andrew.m.fabrizio@gmail.com>
2018-12-02 21:33:48 -0800

    Integrated d3 modules. Connected hierarchySVG to react component lifecycle and props.

--- src/components/SVGs/HierarchySVG.jsx
+++ src/components/SVGs/HierarchySVG.jsx
@@ -1,9 +1,56 @@
+//bl.ocks.org/NPashaP/7683252 
 import React, { Component } from 'react';
+import { select, selection } from 'd3-selection';
 
 export default class HierarchySVG extends Component {
+	constructor(props) {
+		super(props);
+		this.initialize = this.initialize.bind(this);
+		this.SVG = React.createRef();
+		this.state = {
+			cx: props.width / 2,
+			cy: props.height / 2,
+			node: {
+				radius: 15,
+			},
+		};
+	};
+
+	componentDidUpdate() {
+		this.initialize();
+	};
+
+	initialize() {
+		select(this.SVG.current)
+			.append('g')
+			.attr('id', 'lines')
+			.selectAll('line')
+				// .data()
+				// .enter()
+				// .append('line')
+				// .attr('x1', ({ p1 }) => p1.x)
+				// .attr('y1', ({ p1 }) => p1.y)
+				// .attr('x2', ({ p2 }) => p2.x)
+				// .attr('y2', ({ p2 }) => p2.y);
+		select(this.SVG.current)
+			.append('g')
+			.attr('id', 'circles')
+				.selectAll('circle')
+				// .data()
+				// .enter()
+				// .append('circle')
+				// .attr('cx', ({ p }) => p.x)
+				// .attr('cy', ({ p }) => p.y)
+				// .attr('r', this.state.node.radius)
+				// .on('click', (d) => console.log(d));
+	};
+
 	render() {
+		console.log(this.props.dataset);
+
 		return (
-			<svg height='400' width='400'></svg>
+			<svg id="tree" ref={this.SVG} height={this.props.height} width={this.props.width}>
+			</svg>
 		);
 	};
 };
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import HierarchySVG from '../SVGs/HierarchySVG.jsx';
+import dataset from '../../datasets/hierarchy.json';
 
 export default class Main extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	};
+
 	render() {
 		return (
 			<div className='main'>
-				<HierarchySVG></HierarchySVG>
+				<HierarchySVG dataset={dataset} height='400' width='400'></HierarchySVG>
 			</div>
 		);
 	};
