9f0dc2e6caddeeb21201e1a82226773212b84096
cytellix-dev <afabrizio@cytellix.com>
2018-12-04 19:21:03 -0800

    Progress adding logic to convert hierarchy data to a tree data structure with SVG geometries.

--- src/components/SVGs/HierarchySVG.jsx
+++ src/components/SVGs/HierarchySVG.jsx
@@ -1,4 +1,4 @@
-//bl.ocks.org/NPashaP/7683252 
+//bl.ocks.org/NPashaP/7683252
 import React, { Component } from 'react';
 import { select, selection } from 'd3-selection';
 
@@ -8,11 +8,15 @@ export default class HierarchySVG extends Component {
 		this.initialize = this.initialize.bind(this);
 		this.SVG = React.createRef();
 		const radius = 15;
+		const spacing = {
+			dx: 10,
+			dy: 30
+		};
 		this.state = {
 			root: {
 				cx: props.width / 2,
-				cy: props.height / 2,
-			};
+				cy: 30
+			},
 			node: {
 				borderColor: '#000',
 				borderWidth: 2,
@@ -24,8 +28,8 @@ export default class HierarchySVG extends Component {
 				strokeWidth: 2,
 			},
 			offset: {
-				dx: (2 * radius) + 10,
-				dy: (2 * radius) + 30
+				dx: (2 * radius) + spacing.dx,
+				dy: (2 * radius) + spacing.dy 
 			},	
 			nodes: [],
 			edges: []
@@ -37,9 +41,64 @@ export default class HierarchySVG extends Component {
 	};
 
 	computeTree(dataset = []) {
-		let tree = [];
-		const addNode = () => {
+		const adopt = (parent, child) => {
+			if (parent) {
+				console.log('adopting child...')
+				const node = {
+					children: child.children,
+					degree: child.children.length,
+					depth: parent.depth + 1,
+					edges: [],
+					id: child.id,
+					label: child.label,
+					level: parent.level + 1,
+					geometry: {
+						cx: 0,
+						cy: 0,
+						r: this.state.node.radius
+					}
+				};
+				node.children = node.children.map( (child) => adopt(node, child) );
+				return node;
+			} else {
+				console.log('adopting root...')
+				const root = {
+					children: child.children,
+					degree: child.children.length, 
+					depth: 0,
+					edges: [],
+					id: child.id,
+					label: child.label,
+					level: 1,
+					geometry: {
+						cx: this.state.root.cx,
+						cy: this.state.root.cy,
+						r: this.state.node.radius
+					}
+				};
+				root.children = root.children.map( (child) => adopt(root, child) );
+				// parent.degree = parent.children.length;
+				// const iCenter = (parent.degree - 1) / 2;
+				// parent.children.forEach( (child, i) => {
+				// 	const offsetDirection = (i === iCenter) ? 0 : (i < iCenter ? -1 : 1 );
+				// 	const offsetMultiplier = Math.ceil(Math.abs(i - iCenter));
+				// 	console.log(JSON.stringify(child));
+				// 	// child.geometry.cx = parent.geometry.cx + (offsetDirection * offsetMultiplier);
+				// 	// child.geometry.cy = parent.geometry.cy + this.state.offset.dy;
+				// } );
+				return root;
+			}
+			
 		};
+		const deriveGeometries = (tree = []) => {
+			return tree;
+		}
+
+		return dataset.map( (root) => {
+			const tree = adopt(null, root);
+			return deriveGeometries(tree);
+		} );
+			
 	};
 
 	initialize() {
@@ -69,6 +128,8 @@ export default class HierarchySVG extends Component {
 
 	render() {
 		console.log(this.props.dataset);
+		const trees = this.computeTree(this.props.dataset);
+		console.log(trees);
 
 		return (
 			<svg id="tree" ref={this.SVG} height={this.props.height} width={this.props.width}>
--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
+const openSansFont = document.createElement('link');
+openSansFont.setAttribute('rel', 'stylesheet');
+openSansFont.setAttribute('href', 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i');
+document.head.append(openSansFont);
+
 ReactDOM.render(<App />, document.getElementById('root'));
