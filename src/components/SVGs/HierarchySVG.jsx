4a602e3c8dc67657804bb53727a73011f7a046c9
andrew <andrew.m.fabrizio@gmail.com>
2018-12-05 02:27:21 -0800

    progress computing node positioning for hierarchy chart.

--- src/components/SVGs/HierarchySVG.jsx
+++ src/components/SVGs/HierarchySVG.jsx
@@ -5,13 +5,9 @@ import { select, selection } from 'd3-selection';
 export default class HierarchySVG extends Component {
 	constructor(props) {
 		super(props);
-		this.initialize = this.initialize.bind(this);
+		this.initialize = this.redraw.bind(this);
 		this.SVG = React.createRef();
 		const radius = 15;
-		const spacing = {
-			dx: 10,
-			dy: 30
-		};
 		this.state = {
 			root: {
 				cx: props.width / 2,
@@ -28,24 +24,38 @@ export default class HierarchySVG extends Component {
 				strokeWidth: 2,
 			},
 			offset: {
-				dx: (2 * radius) + spacing.dx,
-				dy: (2 * radius) + spacing.dy 
+				dx: (2 * radius) + (this.props.dx || 10), 
+				dy: (2 * radius) + (this.props.dy || 30) 
 			},	
 			nodes: [],
 			edges: []
 		};
 	};
 
-	componentDidUpdate() {
-		this.initialize();
+	componentDidMount() {
+		console.log(this.props.dataset);
+		const trees = this.computeTrees(this.props.dataset);
+		console.log(trees);
+		this.redraw(trees[0]);
 	};
 
-	computeTree(dataset = []) {
-		const adopt = (parent, child) => {
+	componentDidUpdate() {
+		const trees = this.computeTrees(this.props.dataset);
+		this.redraw(trees[0]);
+	};
+
+	computeTrees(dataset = []) {
+		const adopt = (parent, child, i) => {
 			if (parent) {
 				console.log('adopting child...')
+				// if (parent.depth > 0 && i === child.children.length -1) {
+				// 	parent.geometry.cx = parent.geometry.cx + (this.state.offset.dx * Math.floor(child.children.length / 2));
+				// }
+				const iCenter = (parent.degree - 1) / 2;
+				const offsetDirection = (i === iCenter) ? 0 : (i < iCenter ? -1 : 1 );
+				const offsetMultiplier = Math.ceil(Math.abs(i - iCenter));
 				const node = {
-					children: child.children,
+					children: new Array(child.children.length), 
 					degree: child.children.length,
 					depth: parent.depth + 1,
 					edges: [],
@@ -53,17 +63,17 @@ export default class HierarchySVG extends Component {
 					label: child.label,
 					level: parent.level + 1,
 					geometry: {
-						cx: 0,
-						cy: 0,
+						cx: parent.geometry.cx + (offsetDirection * offsetMultiplier * this.state.offset.dx) + (-1 * offsetDirection * (child.children.length % 2 === 0 ? 1 : 0) * (this.state.node.radius + 5)),
+						cy: parent.geometry.cy + this.state.offset.dy,
 						r: this.state.node.radius
 					}
 				};
-				node.children = node.children.map( (child) => adopt(node, child) );
+				node.children = child.children.map( (child, i) => adopt(node, child, i) );
 				return node;
 			} else {
 				console.log('adopting root...')
 				const root = {
-					children: child.children,
+					children: new Array(child.children.length), 
 					degree: child.children.length, 
 					depth: 0,
 					edges: [],
@@ -76,32 +86,25 @@ export default class HierarchySVG extends Component {
 						r: this.state.node.radius
 					}
 				};
-				root.children = root.children.map( (child) => adopt(root, child) );
-				// parent.degree = parent.children.length;
-				// const iCenter = (parent.degree - 1) / 2;
-				// parent.children.forEach( (child, i) => {
-				// 	const offsetDirection = (i === iCenter) ? 0 : (i < iCenter ? -1 : 1 );
-				// 	const offsetMultiplier = Math.ceil(Math.abs(i - iCenter));
-				// 	console.log(JSON.stringify(child));
-				// 	// child.geometry.cx = parent.geometry.cx + (offsetDirection * offsetMultiplier);
-				// 	// child.geometry.cy = parent.geometry.cy + this.state.offset.dy;
-				// } );
+				root.children = child.children.map( (child, i) => adopt(root, child, i) );
 				return root;
 			}
 			
 		};
-		const deriveGeometries = (tree = []) => {
-			return tree;
-		}
-
-		return dataset.map( (root) => {
-			const tree = adopt(null, root);
-			return deriveGeometries(tree);
-		} );
-			
+		return dataset.map( (root) => adopt(null, root, 0) );
 	};
 
-	initialize() {
+	redraw(root) {
+		const extractCircles = (childNodes) => childNodes.reduce(
+			(circles, node) => circles.concat(
+				[ node.geometry ],
+				extractCircles(node.children)
+			),
+			[]
+		);
+		const circles = [ { ...root.geometry } ].concat(extractCircles(root.children));
+		console.log(circles);
+		console.log(this.SVG.current)
 		select(this.SVG.current)
 			.append('g')
 			.attr('id', 'lines')
@@ -117,20 +120,19 @@ export default class HierarchySVG extends Component {
 			.append('g')
 			.attr('id', 'circles')
 			.selectAll('circle')
-				// .data()
-				// .enter()
-				// .append('circle')
-				// .attr('cx', ({ p }) => p.x)
-				// .attr('cy', ({ p }) => p.y)
-				// .attr('r', this.state.node.radius)
-				// .on('click', (d) => console.log(d));
+				.data(circles)
+				.enter()
+				.append('circle')
+				.attr('cx', ({ cx }) => cx)
+				.attr('cy', ({ cy }) => cy)
+				.attr('r', ({ r }) => r)
+				.style('fill', this.state.node.fill)
+				.style('stroke', this.state.node.borderColor)
+				.style('strokeWidth', this.state.node.borderWidth)
+				.on('click', (d) => console.log(d));
 	};
 
 	render() {
-		console.log(this.props.dataset);
-		const trees = this.computeTree(this.props.dataset);
-		console.log(trees);
-
 		return (
 			<svg id="tree" ref={this.SVG} height={this.props.height} width={this.props.width}>
 			</svg>
