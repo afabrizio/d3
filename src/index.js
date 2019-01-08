e0ba66d4f641321cac2b3b26c4df553234f23ce5
cytellix-dev <afabrizio@cytellix.com>
2019-01-08 15:33:12 -0800

    Updated configuration for local build support in addition to development server support.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App.jsx';
 const openSansFont = document.createElement('link');
 openSansFont.setAttribute('rel', 'stylesheet');
 openSansFont.setAttribute('href', 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i');
-document.head.append(openSansFont);
+document.head.appendChild(openSansFont);
+const rootElement = document.createElement('div');
+rootElement.setAttribute('id', 'root');
+document.body.appendChild(rootElement);
 
 ReactDOM.render(<App />, document.getElementById('root'));
